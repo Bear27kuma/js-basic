7141dec080a1b38eba9a19cd93715825eeeabcdd
Yuta Okuma <y.dream.nest@icloud.com>
2022-07-28 21:19:40 +0900

    TODOの完了機能

--- src/index.js
+++ src/index.js
@@ -12,17 +12,26 @@ const onClickAdd = () => {
   // 完了ボタンを生成する
   const completeButton = document.createElement('button');
   completeButton.innerText = '完了';
-  completeButton.addEventListener('click', () => {
-    alert('完了');
-  });
-
   // 削除ボタンを生成する
   const deleteButton = document.createElement('button');
   deleteButton.innerText = '削除';
+  // 戻すボタンを生成する
+  const backButton = document.createElement('button');
+  backButton.innerText = '戻す';
+
+  // 完了ボタンを押された時に挙動
+  completeButton.addEventListener('click', () => {
+    // 押された完了ボタンの親タグ(li)を未完了リストから削除する
+    deleteFromIncompleteList(deleteButton.parentNode);
+
+    // 押された完了ボタンの親タグ(li)を未完了リストから完了リストに移動する
+    addTargetFromIncompleteList(completeButton.parentNode);
+  });
+
+  // 削除ボタンを押された時に挙動
   deleteButton.addEventListener('click', () => {
-    // 押された削除ボタンの親タグ(li)を未完了リストから削除
-    const deleteTarget = deleteButton.parentNode;
-    document.getElementById('incomplete-list').removeChild(deleteTarget);
+    // 押された削除ボタンの親タグ(li)を未完了リストから削除する
+    deleteFromIncompleteList(deleteButton.parentNode);
   });
 
   // liタグの子要素に各要素を設定する
@@ -35,6 +44,32 @@ const onClickAdd = () => {
 
   // テキストボックスを初期化する
   input.value = '';
+}
+
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById('incomplete-list').removeChild(target);
+}
+
+// 未完了リストから完了リストへ指定の要素を移動する
+const addTargetFromIncompleteList = (target) => {
+  // pタグ（最初の要素）を取得する
+  const text = target.firstElementChild;
+
+  // liを生成する
+  const li = document.createElement('li');
+  // 戻すボタンを生成する
+  const backButton = document.createElement('button');
+  backButton.innerText = '戻す';
+
+  // 各要素を子要素に設定する
+  li.appendChild(text);
+  li.appendChild(backButton);
+
+  console.log(li);
+
+  // 完了リストに追加する
+  document.getElementById('complete-list').appendChild(li);
 }
 
 // 追加ボタンをクリックする
