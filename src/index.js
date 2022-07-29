845aa20184dcb8ba76f0027527b3638a6bea1d20
Yuta Okuma <y.dream.nest@icloud.com>
2022-07-29 09:14:58 +0900

    TODOの戻す機能

--- src/index.js
+++ src/index.js
@@ -19,19 +19,25 @@ const onClickAdd = () => {
   const backButton = document.createElement('button');
   backButton.innerText = '戻す';
 
-  // 完了ボタンを押された時に挙動
+  // 完了ボタンが押された時の挙動
   completeButton.addEventListener('click', () => {
     // 押された完了ボタンの親タグ(li)を未完了リストから削除する
     deleteFromIncompleteList(deleteButton.parentNode);
 
     // 押された完了ボタンの親タグ(li)を未完了リストから完了リストに移動する
-    addTargetFromIncompleteList(completeButton.parentNode);
+    addTargetFromIncompleteList(completeButton.parentNode, backButton);
   });
 
-  // 削除ボタンを押された時に挙動
+  // 削除ボタンが押された時の挙動
   deleteButton.addEventListener('click', () => {
     // 押された削除ボタンの親タグ(li)を未完了リストから削除する
     deleteFromIncompleteList(deleteButton.parentNode);
+  });
+
+  // 戻すボタンが押された時の挙動
+  backButton.addEventListener('click', () => {
+    // 未完了リストへ移動させる
+    backTargetToIncompleteList(backButton.parentNode, completeButton, deleteButton);
   });
 
   // liタグの子要素に各要素を設定する
@@ -52,24 +58,38 @@ const deleteFromIncompleteList = (target) => {
 }
 
 // 未完了リストから完了リストへ指定の要素を移動する
-const addTargetFromIncompleteList = (target) => {
+const addTargetFromIncompleteList = (target, backButton) => {
   // pタグ（最初の要素）を取得する
   const text = target.firstElementChild;
 
   // liを生成する
   const li = document.createElement('li');
-  // 戻すボタンを生成する
-  const backButton = document.createElement('button');
-  backButton.innerText = '戻す';
 
   // 各要素を子要素に設定する
   li.appendChild(text);
   li.appendChild(backButton);
 
-  console.log(li);
-
   // 完了リストに追加する
   document.getElementById('complete-list').appendChild(li);
+}
+
+// 完了した要素を未完了リストへ移動する
+const backTargetToIncompleteList = (target, completeButton, deleteButton) => {
+  document.getElementById('complete-list').removeChild(target);
+
+  // pタグを取得する
+  const text = target.firstElementChild;
+
+  // liを生成する
+  const li = document.createElement('li');
+
+  // 各要素を子要素に設定する
+  li.appendChild(text);
+  li.appendChild(completeButton);
+  li.appendChild(deleteButton);
+
+  // 未完了リストに追加する
+  document.getElementById('incomplete-list').appendChild(li);
 }
 
 // 追加ボタンをクリックする
